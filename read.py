65b6ffb9e621a2cb7bac3385b738809accceb05b
yvette chen <annd840214@gmail.com>
2021-10-07 13:09:25 +0800

    calc average of reviews

--- read.py
+++ read.py
@@ -20,3 +20,13 @@ sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
 print("平均留言長度是：", sum_len / len(data))
+
+#teacher 篩選
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print("一共有", len(new), "筆留言長度小於100")
+
+
+ 
